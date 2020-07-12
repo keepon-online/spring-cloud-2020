9af6863608388942921ab784e2d8f8454c1fd400
jieepre <jieepre@gmail.com>
2020-07-13 01:47:18 +0800

    :tada: init

---
+++ cloud-provider-payment8001/src/main/java/online/keepon/springcloud/Payment8001.java
@@ -0,0 +1,13 @@
+package online.keepon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Payment8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001.class, args);
+    }
+}
